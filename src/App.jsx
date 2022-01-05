62842e2d4e7bebfdacef8949e0d55e65470edbe9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-05 14:53:28 -0800

    setting up basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 export default function App() {
-  return <h1>Hello World</h1>;
+  return ( 
+  <Router>
+    <Switch>
+      <Route>
+        <Home/>
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
